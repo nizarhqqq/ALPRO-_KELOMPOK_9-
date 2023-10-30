18b657802f72bd27001ec873e6c429df13a53409
nizarhqqq <149160035+nizarhqqq@users.noreply.github.com>
2023-10-30 12:52:08 +0700

    Create orderpizzaa.py

---
+++ orderpizzaa.py
@@ -0,0 +1,37 @@
+print("Selamat datang di Toko pizza D4MI ")
+
+print("Frankfurter")
+print("Meat Monsta")
+print("Super Supreme")
+print("Super Supreme Chicken")
+
+TopinggPizza = input("ingin pizza virian apa ?")
+hargaPizza = 0
+if TopinggPizza == "Frankfurter":
+    hargatoping = 35000
+elif TopinggPizza == "Meat monsta":
+    hargatoping = 30000
+elif TopinggPizza == "Super Supreme":
+    hargatoping = 40000
+elif TopinggPizza == "Super Supreme Chicken":
+    hargatoping = 50000
+else:
+    print("varian tidak ada di menu")
+
+print("Pan Crust")
+print("Stuffed Crust")
+print("Crown Crust")
+print("Cheesy Bire")
+
+crustpizza = input("Silakan pilih Crush Pizza")
+harga_crustpizza = 0
+if crustpizza == "Pan Crust":
+    harga_crustpizza = 5000
+elif crushpizza == "Stuffed Crust":
+    harga_crustpizza = 6000
+elif crustpizza == "Crown crust":
+    harga_crustpizza = 40000
+elif crustpizza == "Cheesy bite":
+    harga_crustpizza = 50000
+else:
+    print("tidak ada di nemu")
