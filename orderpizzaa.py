a1f558a264a5418072e4e6846ee5f7b49eecc83e
nizarhqqq <149160035+nizarhqqq@users.noreply.github.com>
2023-10-30 20:55:07 +0700

    orderpizzaa.py

--- orderpizzaa.py
+++ orderpizzaa.py
@@ -1,37 +1,39 @@
-print("Selamat datang di Toko pizza D4MI ")
+print("Selamat datang di Toko pizza D4MI\n")
 
 print("Frankfurter")
 print("Meat Monsta")
 print("Super Supreme")
 print("Super Supreme Chicken")
 
-TopinggPizza = input("ingin pizza virian apa ?")
+TopinggPizza = input("\ningin pizza varian apa ? :")
 hargaPizza = 0
 if TopinggPizza == "Frankfurter":
     hargatoping = 35000
-elif TopinggPizza == "Meat monsta":
+elif TopinggPizza == "Meat Monsta":
     hargatoping = 30000
 elif TopinggPizza == "Super Supreme":
     hargatoping = 40000
 elif TopinggPizza == "Super Supreme Chicken":
     hargatoping = 50000
 else:
-    print("varian tidak ada di menu")
+    print("\nToping tidak ada")
+
+print("=====================================\n")
 
 print("Pan Crust")
 print("Stuffed Crust")
 print("Crown Crust")
 print("Cheesy Bire")
 
-crustpizza = input("Silakan pilih Crush Pizza")
+crustpizza = input("\nSilakan pilih Crush Pizza :")
 harga_crustpizza = 0
 if crustpizza == "Pan Crust":
     harga_crustpizza = 5000
-elif crushpizza == "Stuffed Crust":
+elif crustpizza == "Stuffed Crust":
     harga_crustpizza = 6000
-elif crustpizza == "Crown crust":
+elif crustpizza == "Crown Crust":
     harga_crustpizza = 40000
-elif crustpizza == "Cheesy bite":
+elif crustpizza == "Cheesy Bites":
     harga_crustpizza = 50000
 else:
-    print("tidak ada di nemu")
+    print("PESANAN ANDA TIDAK VALID")
